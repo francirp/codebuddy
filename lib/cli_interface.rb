c076d6c3b4905e40eb91528a750b44ef8cdd59fa
Ryan Francis <ryan.p.francis@gmail.com>
2023-11-09 10:59:10 -0600

    updated cli_interface

--- lib/cli_interface.rb
+++ lib/cli_interface.rb
@@ -1,34 +1,61 @@
 require 'thor'
+require 'json'
+require_relative 'context_manager'
+require_relative 'chat_gpt_assistant'
 
 class CLIInterface < Thor
-  desc "init", "Initialize Jarvis in the current directory"
-  def init
-    # Initialization code here
-    puts "Jarvis initialized."
+  CONFIG_FILE = File.join(Dir.home, '.codebuddy_config.json')
+
+  def initialize(*args)
+    super
+    @context_manager = ContextManager.new
+    @config = load_config
+    @chat_gpt_assistant = ChatGPTAssistant.new(api_key: @config['openai_key'])
   end
 
-  desc "add_context DIRECTORY", "Add a directory to Jarvis's context"
+  desc "init", "Initialize Codebuddy in the current directory"
+  def init
+    puts "Initializing Codebuddy..."
+    # Perform initialization steps
+    puts "Codebuddy initialized."
+  end
+
+  desc "set_openai_key KEY", "Set your OpenAI API key"
+  def set_openai_key(key)
+    @config['openai_key'] = key
+    save_config
+    puts "OpenAI API key updated."
+  end
+
+  desc "add_context DIRECTORY", "Add a directory to Codebuddy's context"
   def add_context(directory)
-    # Code to add a directory to the context
+    @context_manager.add_directory(directory)
     puts "#{directory} added to context."
   end
 
-  desc "remove_context DIRECTORY", "Remove a directory from Jarvis's context"
+  desc "remove_context DIRECTORY", "Remove a directory from Codebuddy's context"
   def remove_context(directory)
-    # Code to remove a directory from the context
+    @context_manager.remove_directory(directory)
     puts "#{directory} removed from context."
   end
 
-  desc "ask QUERY", "Send a query to Jarvis"
+  desc "ask QUERY", "Send a query to Codebuddy"
   def ask(query)
-    # Code to send the query to ChatGPT Assistant and display the response
-    puts "Query sent: #{query}"
-    # Display the response here
+    response = @chat_gpt_assistant.send_request(query)
+    puts "Response:\n#{response}"
   end
-
-  # Additional commands can be added here
 
   private
 
-  # Private helper methods can be defined here
+  def save_config
+    File.write(CONFIG_FILE, JSON.pretty_generate(@config))
+  end
+
+  def load_config
+    if File.exist?(CONFIG_FILE)
+      JSON.parse(File.read(CONFIG_FILE))
+    else
+      {}
+    end
+  end
 end
